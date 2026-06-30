683373743bc6024e91459fc7b291d1f7ea29f77f
0dinsky <alex.v.ledenev234@gmail.com>
2026-06-30 10:50:59 +0300

    Update crypto_setup.go

--- internal/handshake/crypto_setup.go
+++ internal/handshake/crypto_setup.go
@@ -28,8 +28,8 @@ var QUICVersionContextKey = &quicVersionContextKey{}
 const clientSessionStateRevision = 5
 
 type cryptoSetup struct {
-	tlsConf *tls.Config
-	conn    quicTLSConn
+	tlsConf            *tls.Config
+	conn               quicTLSConn
 	clientRandomPrefix []byte
 	clientRandomMask   []byte
 
@@ -111,7 +111,6 @@ func NewCryptoSetupClient(
 		// standard Go TLS ClientHello-building path is used, which correctly
 		// calls quicGetTransportParameters() and sets hello.quicTransportParameters.
 		id := utls.HelloGolang
-		logger.Debugf("NewCryptoSetupClient: using utlsQUICConn HelloGolang, prefix[:4]=%x", clientRandomPrefix[:min(4, len(clientRandomPrefix))])
 		cs.conn = newUTLSQUICConn(tlsConf, id, clientRandomPrefix, clientRandomMask, logger)
 	} else {
 		cs.conn = &stdQUICConn{tls.QUICClient(&tls.QUICConfig{
